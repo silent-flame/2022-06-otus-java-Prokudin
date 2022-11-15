a259a5923af8dbd6605b0abcd4254f063989b226
silentflame <bormotan1@yandex.ru>
2022-11-16 01:41:13 +0300

    homework 13/ IoC

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -1,5 +1,6 @@
 public interface Versions {
     String guava = "31.1-jre";
+    String reflections = "0.10.2";
     String jetty = "11.0.7";
     String freemarker = "2.3.31";
 }
---
+++ hw13-gradle/src/main/java/ru/otus/utils/ReflectionUtils.java
@@ -0,0 +1,39 @@
+package ru.otus.utils;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ReflectionUtils {
+    public static Set<Class> findAllClasses(String packageName) {
+        InputStream stream = ClassLoader.getSystemClassLoader()
+                .getResourceAsStream(packageName.replaceAll("[.]", "/"));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+
+        Set<Class> classes = new HashSet<>();
+        reader.lines().forEach(line -> {
+            if (line.endsWith(".class")) {
+                classes.add(getClass(line, packageName));
+            } else {
+                classes.addAll(findAllClasses(packageName + "." + line));
+            }
+        });
+
+        return classes;
+    }
+
+    private static Class getClass(String className, String packageName) {
+        try {
+            return Class.forName(packageName + "."
+                    + className.substring(0, className.lastIndexOf('.')));
+        } catch (ClassNotFoundException e) {
+            // handle the exception
+        }
+        return null;
+    }
+}
