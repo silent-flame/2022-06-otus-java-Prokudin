04b6aecddb124faef525b903eae2ae0922c4549e
silentflame <bormotan1@yandex.ru>
2023-03-29 03:40:29 +0300

    homework 18/webflux-websocket chat

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -3,4 +3,8 @@ public interface Versions {
     String reflections = "0.10.2";
     String jetty = "11.0.7";
     String freemarker = "2.3.31";
+    String r2dbcPostgresql = "0.8.13.RELEASE";
+    String sockjs = "1.5.1";
+    String stomp = "2.3.4";
+    String bootstrap = "5.2.3";
 }
